fb7a375ee1424367c309cbf086581dc857347b81
SHYAKAgreat <72033556+SHYAKAgreat@users.noreply.github.com>
2020-10-08 08:05:52 +0200

    Create socialq.js

---
+++ socialq.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "what is motto of CSA?",
+    answer: "Uburere Ubutwari Ubumenyi",
+    options: [
+      "Uburere ubuzima ubumenyi",
+      "Uburere Ubutwari Ubumenyi",
+      "Discipline and Success",
+      "Sports and education"
+    ]
+  },
+    {
+    numb: 2,
+    question: "who won nobel peace prize 2019?",
+    answer: "Abiy ahmed",
+    options: [
+      "Nelson mandela",
+      "Poul Kagame",
+      "Abiy ahmed",
+      "none of them"
+    ]
+  },
+    {
+    numb: 3,
+    question: "who is current first billionaire 2020?",
+    answer: "Jeff bezos",
+    options: [
+      "Jeff Bezos",
+      "Steve jobs",
+      "Bill gates",
+      "mark Zuckerberg"
+    ]
+  },
+    {
+    numb: 4,
+    question: "what is earth's current population 2020?",
+    answer: "7.8 billions",
+    options: [
+      "10 billions",
+      "100 millions",
+      "7.8 billions",
+      "all of them"
+    ]
+  },
+    {
+    numb: 5,
+    question: "who are creators of this website?",
+    answer: "SHYAKA Aimé & Tony Robert",
+    options: [
+      "Kalisa and Matayo",
+      "SHYAKA Aimé & Tony Robrt",
+      "CSA IT",
+      "None of them"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
