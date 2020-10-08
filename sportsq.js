a595c5a5c97678f70769cc6357ac91da8d573421
SHYAKAgreat <72033556+SHYAKAgreat@users.noreply.github.com>
2020-10-08 08:30:17 +0200

    Add files via upload

---
+++ sportsq.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "is it good to do sports?",
+    answer: "Yes",
+    options: [
+      "No",
+      "Yes",
+      "i dont't know",
+      "may be.."
+    ]
+  },
+    {
+    numb: 2,
+    question: "How do we call sports competition in CSA?",
+    answer: "MAGIS",
+    options: [
+      "CSA Ligue",
+      "MAGIS",
+      "Vurgus",
+      "none of them"
+    ]
+  },
+    {
+    numb: 3,
+    question: "How many ballon D'or messi have?",
+    answer: "6",
+    options: [
+      "1",
+      "0",
+      "6",
+      "100"
+    ]
+  },
+    {
+    numb: 4,
+    question: "which country have many football world cups?",
+    answer: "BRAZIL",
+    options: [
+      "RWANDA",
+      "URUGUAY",
+      "GERMANY",
+      "none of them"
+    ]
+  },
+    {
+    numb: 5,
+    question: "what is the name of basketball ligue in USA?",
+    answer: "NBA",
+    options: [
+      "FERWABA",
+      "NBA",
+      "UBA",
+      "CBA"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
