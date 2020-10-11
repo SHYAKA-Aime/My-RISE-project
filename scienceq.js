e185b426aabf5c1fa4edb2610a246549ebb2352a
SHYAKAgreat <72033556+SHYAKAgreat@users.noreply.github.com>
2020-10-11 11:45:41 +0200

    Update scienceq.js

--- scienceq.js
+++ scienceq.js
@@ -48,7 +48,7 @@ let questions = [
   },
     {
     numb: 5,
-    question: "what is the exact value of <b>&pi;</b>of circle?",
+    question: "what is the exact value of <b>&pi;</b> of circle?",
     answer: "22/7",
     options: [
       "100",
