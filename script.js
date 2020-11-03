c04a52c09f71ef78e274e571efa068abcdb5c8c6
SHYAKA-Aime <72033556+SHYAKA-Aime@users.noreply.github.com>
2020-11-03 14:27:01 +0200

    Update script.js

--- script.js
+++ script.js
@@ -154,15 +154,15 @@ function showResult(){
     const scoreText = result_box.querySelector(".score_text");
     if (userScore > 3){ // if user scored more than 3
         //creating a new span tag and passing the user score number and total question number
-        let scoreTag = '<span>and congrats! 🎉,You got <p>'+ userScore +'</p> out of<p>'+ questions.length +'</p></span>';
+        let scoreTag = '<span>and congrats! 🎉,You got <p>'+ userScore +'</p>&nbsp; out of &nbsp;<p>'+ questions.length +'</p></span>';
         scoreText.innerHTML = scoreTag;  //adding new span tag inside score_Text
     }
     else if(userScore > 1){ // if user scored more than 1
-        let scoreTag = '<span>and nice 😎, You got <p>'+ userScore +'</p> out of <p>'+ questions.length +'</p></span>';
+        let scoreTag = '<span>and nice 😎, You got <p>'+ userScore +'</p>&nbsp;out of &nbsp;<p>'+ questions.length +'</p></span>';
         scoreText.innerHTML = scoreTag;
     }
     else{ // if user scored less than 1
-        let scoreTag = '<span>and sorry 😐, You got only<p>'+ userScore +'out of <p>'+ questions.length +'</p></span>';
+        let scoreTag = '<span>and sorry 😐, You got only<p>'+ userScore +'&nbsp;out of&nbsp;<p>'+ questions.length +'</p></span>';
         scoreText.innerHTML = scoreTag;
     }
 }
