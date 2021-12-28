853ae3492e9ccc659eadc204e46b897f74218f89
Aime Brues <72033556+SHYAKA-Aime@users.noreply.github.com>
2021-12-28 19:14:39 +0200

    Create grade4.js

---
+++ grade4.js
@@ -0,0 +1,72 @@
+//By SHYAKA Aimé ©
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Who is the CEO of google?",
+    answer: "Sundar pichai",
+    options: [
+      "Larry page",
+      "Mark zuckerberg",
+      "Sundar pichai",
+      "Sergey Brin"
+    ]
+  },
+    {
+    numb: 2,
+    question: "who won nobel peace prize 2019?",
+    answer: "Abiy ahmed",
+    options: [
+      "Nelson mandela",
+      "Poul Kagame",
+      "Abiy ahmed",
+      "none of them"
+    ]
+  },
+    {
+    numb: 3,
+    question: "who is current first billionaire 2020?",
+    answer: "Jeff Bezos",
+    options: [
+      "Jeff Bezos",
+      "Steve jobs",
+      "Bill gates",
+      "mark Zuckerberg"
+    ]
+  },
+    {
+    numb: 4,
+    question: "what is earth's current population 2020?",
+    answer: "7.8 billions",
+    options: [
+      "10 billions",
+      "100 millions",
+      "7.8 billions",
+      "all of them"
+    ]
+  },
+    {
+    numb: 5,
+    question: "capital city of Tanzania is?",
+    answer: "Dodoma",
+    options: [
+      "Kigali",
+      "Dodoma",
+      "Dar es Salam",
+      "None of them"
+    ]
+  },
+   // i can increase number of questions up to infinity 😂😁😊
+
+  //   {
+  //   numb: 6,
+  //   question: "Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
